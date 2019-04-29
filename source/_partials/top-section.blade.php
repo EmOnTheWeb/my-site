c960922712764fac6e6e3bae64186efd5075a83b
emilie <emilie.dannenberg@gmail.com>
2019-04-29 12:28:47 -0500

    add breakpoints and background image

---
+++ source/_partials/top-section.blade.php
@@ -0,0 +1,6 @@
+<div class="overlay">
+	<picture>
+		  <source media="(min-width: 768px)" srcset="../assets/images/bridge-background_2000.jpg" />
+		  <img src="images/horses_small.jpg" alt="Horses in Hawaii">
+	</picture>
+</div>
