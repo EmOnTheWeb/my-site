36c1ec2c517cef7ce229386148b2cf5883d32a79
emilie <emilie.dannenberg@gmail.com>
2019-04-29 12:50:49 -0500

    add mobile img

--- source/_partials/top-section.blade.php
+++ source/_partials/top-section.blade.php
@@ -1,6 +1,6 @@
 <div class="overlay">
 	<picture>
-		  <source media="(min-width: 768px)" srcset="../assets/images/bridge-background_2000.jpg" />
-		  <img src="images/horses_small.jpg" alt="Horses in Hawaii">
+		  <source media="(min-width: 768px)" srcset="../assets/images/background-bridge_2000.jpg" />
+		  <img src="../assets/images/background-bridge_mobile.jpg" alt="image of a bridge in New Orleans">
 	</picture>
 </div>
