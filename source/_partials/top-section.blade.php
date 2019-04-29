94ede5a8a925fc15b3b38937efd650c019a75fc8
emilie <emilie.dannenberg@gmail.com>
2019-04-29 13:00:45 -0500

    fix overlay

--- source/_partials/top-section.blade.php
+++ source/_partials/top-section.blade.php
@@ -1,6 +1,7 @@
-<div class="overlay">
+<div class="background-container">
 	<picture>
 		  <source media="(min-width: 768px)" srcset="../assets/images/background-bridge_2000.jpg" />
 		  <img src="../assets/images/background-bridge_mobile.jpg" alt="image of a bridge in New Orleans">
+		  <div class="overlay"></div>
 	</picture>
 </div>
