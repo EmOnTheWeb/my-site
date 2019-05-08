f16617961a34411d81627e6c37d5a247ad07a839
emilie <emilie.dannenberg@gmail.com>
2019-05-08 14:11:51 -0500

    add portfolio links

--- source/_partials/portfolio-section.blade.php
+++ source/_partials/portfolio-section.blade.php
@@ -1,77 +1,91 @@
-<!-- <section class="container light-gray"> -->
+<section class="container light-gray">
 	<h2 class="heading">Portfolio -</h2>
 
 	<div class="portfolio-slider">
 		<div class="portfolio-item">
+			<a href="https://kotlikoff.net/" target="blank">
 			<div class="image" style="background-image:url('../assets/images/portfolio/kotlikoff_img.jpg')"></div>
 			<div class="portfolio-item__text">
 				<h3>Kotlikoff -</h3>
 				
 				<p>Backend developer for this bespoke Wordpress theme using Sage, WP Controller and Advanced Custom Fields. Various custom post types.
 				</p>
-				<span class="tag">Backend CMS</span><span class="tag">Wordpress</span>
+				<div class="tag-container"><span class="tag">Backend CMS</span><span class="tag">Wordpress</span></div>
 			</div>
+			</a>
 		</div>
 		<div class="portfolio-item">
+			<a href="https://www.strettons.co.uk" target="blank">
 			<div class="image" style="background-image:url('../assets/images/portfolio/strettons_img.jpg')"></div>
 			<div class="portfolio-item__text">
 				<h3>Strettons -</h3>
 				
 				<p>Backend developer for this real estate website. Integrates with 3rd party software through APIs / JSON feeds to show property listings. 
 				</p>
-				<span class="tag">Backend CMS</span><span class="tag">Silverstripe</span>
+				<div class="tag-container"><span class="tag">Backend CMS</span><span class="tag">Silverstripe</span></div>
 			</div>
+			</a>
 		</div>
 		<div class="portfolio-item">
+			<a href="http://nsc.emiliedannenberg.com/" target="blank">
 			<div class="image" style="background-image:url('../assets/images/portfolio/nsc_img.jpg')"></div>
 			<div class="portfolio-item__text">
 				<h3>Newcastle Sleep Clinic -</h3>
 				
 				<p>Waiting on client for content. Designed this website according to client brief (cloud image, clean) after they were fired by their previous web designer. Also built it.
 				</p>
-				<span class="tag">Design</span><span class="tag">Development</span><span class="tag">Wordpress</span>
+				<div class="tag-container"><span class="tag">Design</span><span class="tag">Development</span><span class="tag">Wordpress</span></div>
 			</div>
+			</a>
 		</div>
 		<div class="portfolio-item">
+			<a href="http://projectmatch.org/" target="blank">
 			<div class="image" style="background-image:url('../assets/images/portfolio/project_match_img.jpg')"></div>
 			<div class="portfolio-item__text">
 				<h3>Project Match -</h3>
 				
 				<p>Extended existing codebase and added new features to this web app that matches trainees to researchers in the medical field.
 				</p>
-				<span class="tag">MVC</span><span class="tag">Codeigniter</span>
+				<div class="tag-container"><span class="tag">MVC</span><span class="tag">Codeigniter</span></div>
 			</div>
+			</a>
 		</div>
 		<div class="portfolio-item">
+			<a href="http://www.happyfaceclicker.com/" target="blank">
 			<div class="image" style="background-image:url('../assets/images/portfolio/face_clicker_img.jpg')"></div>
 			<div class="portfolio-item__text">
 				<h3>Happy Face Clicker -</h3>
 				
 				<p>Inspired by the BBC documentary 'Rainy Brain Sunny Brain' built a web version of this positivity game where you click on the happy face. 
 				</p>
-				<span class="tag">MVC</span><span class="tag">Express</span><span class="tag">MEAN Stack</span>
+				<div class="tag-container"><span class="tag">MVC</span><span class="tag">Express</span><span class="tag">MEAN Stack</span></div>
 			</div>
+			</a>
 		</div>
 		<div class="portfolio-item">
-			<div class="image" style="background-image:url('../assets/images/portfolio/kotlikoff_img.jpg')"></div>
+			<a href="https://itunes.apple.com/us/app/jumpq/id1359695673#?platform=iphone" target="blank">
+			<div class="image" style="background-image:url('../assets/images/portfolio/jumpq_img.jpg')"></div>
 			<div class="portfolio-item__text">
 				<h3>JumpQ -</h3>
 				
 				<p>From first line of code to deployment to App store & Google play, built this mobile app for preordering food from a Danish airport. Integrates with a Rails backend. 
 				</p>
-				<span class="tag">Mobile Development</span><span class="tag">Ionic 3</span>
+				<div class="tag-container"><span class="tag">Mobile Development</span><span class="tag">Ionic 3</span></div>
 			</div>
+			</a>
 		</div>
 		<div class="portfolio-item">
-			<div class="image" style="background-image:url('../assets/images/portfolio/kotlikoff_img.jpg')"></div>
+			<a href="" target="blank">
+			<div class="image" style="background-image:url('../assets/images/portfolio/walks_img.jpg')"></div>
 			<div class="portfolio-item__text">
 				<h3>Hidden Walks -</h3>
 				
 				<p>Hobby project (no link sorry) A tour guide app that triggers audio based on GPS coordinates. Code is done but need a historian to provide some content.
 				</p>
-				<span class="tag">Mobile development</span><span class="tag">Ionic 3</span><span class="tag">React Native</span>
+				<div class="tag-container"><span class="tag">Mobile development</span><span class="tag">Ionic 3</span><span class="tag">React Native</span></div>
 			</div>
+			</a>
 		</div>
 		
 	</div>	
-<!-- </section> -->
+</section>
