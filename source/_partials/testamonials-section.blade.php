dd2886a0d6f6c891a21abb3704473bf6e8535a93
emilie <emilie.dannenberg@gmail.com>
2019-05-08 14:13:00 -0500

    add a nbsp

--- source/_partials/testamonials-section.blade.php
+++ source/_partials/testamonials-section.blade.php
@@ -14,7 +14,7 @@
 			<div>
 				<p>
 					<i class="fa fa-quote-left" aria-hidden="true"></i>
-					Emilie is by far the most expedient, professional, and competent contractor I've worked with.
+					&nbsp;Emilie is by far the most expedient, professional, and competent contractor I've worked with.
 					<i class="fa fa-quote-right" aria-hidden="true"></i>
 
 					<span>Kathy Hammond, MedFit Staffing Solutions</span>
