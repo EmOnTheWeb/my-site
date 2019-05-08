8b2399114d99527353615bb50d2d9eb2b4ec573a
emilie <emilie.dannenberg@gmail.com>
2019-05-08 12:16:24 -0500

    initialise testamonials slider

--- source/_partials/testamonials-section.blade.php
+++ source/_partials/testamonials-section.blade.php
@@ -1,19 +1,25 @@
 <section class="container dark">
 	<div class="container-content">
 		<h2 class="heading">Testamonials -</h2>
-		<p>	
-			<i class="fa fa-quote-left" aria-hidden="true"></i>
-			&nbsp;Emilie is a talented full stack developer that we've worked with for several years. She's totally reliable, communicates well and is committed to staying up to date with technology advances and methodology improvements. We recommend her whole-heartedly.
-			<i class="fa fa-quote-right" aria-hidden="true"></i> 
+		<div class="testamonials-slider">
+			<div>
+				<p>	
+					<i class="fa fa-quote-left" aria-hidden="true"></i>
+					&nbsp;Emilie is a talented full stack developer that we've worked with for several years. She's totally reliable, communicates well and is committed to staying up to date with technology advances and methodology improvements. We recommend her whole-heartedly.
+					<i class="fa fa-quote-right" aria-hidden="true"></i> 
 
-			<span>Nick Ellison, Managing Director, Purr Group</span> 
-		</p>
-		<p>
-			<i class="fa fa-quote-left" aria-hidden="true"></i>
-			Emilie is by far the most expedient, professional, and competent contractor I've worked with.
-			<i class="fa fa-quote-right" aria-hidden="true"></i>
+					<span>Nick Ellison, Managing Director, Purr Group</span> 
+				</p>
+			</div>
+			<div>
+				<p>
+					<i class="fa fa-quote-left" aria-hidden="true"></i>
+					Emilie is by far the most expedient, professional, and competent contractor I've worked with.
+					<i class="fa fa-quote-right" aria-hidden="true"></i>
 
-			<span>Kathy Hammond, MedFit Staffing Solutions</span>
-		</p>
+					<span>Kathy Hammond, MedFit Staffing Solutions</span>
+				</p>
+			</div>
+		</div>
 	</div>
 </section>
