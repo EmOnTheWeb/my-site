25003eed058f895e24364bd20991c75704f77d60
emilie <emilie.dannenberg@gmail.com>
2019-05-09 12:07:00 -0500

    adding new top section

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="light-gray">
-	<span class="copyright"><i class="fa fa-copyright"></i> made by me</span>
+	<span class="copyright"><i class="fa fa-copyright"></i> made by me using Jigsaw static site generator and Netlify</span>
 	<span class="social-links">
 		<i class="fa fa-linkedin-square"></i>
 		<i class="fa fa-github"></i>
--- source/_partials/top-section.blade.php
+++ source/_partials/top-section.blade.php
@@ -1,13 +1,16 @@
 <div class="background-container">
-	<picture>
-	    <source media="(min-width: 768px)" srcset="../assets/images/background-bridge_2000.jpg" />
-	    <img src="../assets/images/background-bridge_mobile.jpg" alt="image of a bridge in New Orleans">
-	    <div class="overlay"></div>
-	</picture>
-	<img class="laptop" src="../assets/images/laptop.png" alt="laptop image">
-	<div class="laptop-content">
-<!-- 		<p>Hello my name is Emilie<br>
-		I make websites and apps</p> -->
-	
+	<div class="intro-text">
+	why do i not take up any width?
 	</div>
+	<picture>
+	    <source media="(min-width: 768px)" srcset="../assets/images/port-bw_1280.jpg" />
+	    <img src="../assets/images/port-bw_mobile.jpg" alt="image of a bridge in New Orleans">
+	  <!--   <div class="overlay"></div> -->
+	</picture>
+<!-- 	<img class="laptop" src="../assets/images/laptop.png" alt="laptop image">
+	<div class="laptop-content">
+	<p>Hello my name is Emilie<br>
+		I make websites and apps</p> 
+	
+	</div> -->
 </div>
